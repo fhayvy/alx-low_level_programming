9903b97a8b0508d9588f9646403e8678e61abdda
Favour Eze <ezefavourchiamaka01@gmail.com>
2023-07-13 16:07:58 +0000

    puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
-int main () {
-    char first_variable[] = "Programming is like building a multilingual puzzle\n";
+int main ()
+{
+    char first_variable[] = "\"Programming is like building a multilingual puzzle\n";
 
     puts(first_variable);
 
