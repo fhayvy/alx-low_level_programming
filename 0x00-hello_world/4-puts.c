0d0f0de0752ec3d3599df38dd755f77a2a563c49
Favour Eze <ezefavourchiamaka01@gmail.com>
2023-07-13 16:27:55 +0000

    added betty style

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,13 @@
 #include<stdio.h>
+
+/*
+ * main is the entry point
+ * and it returns 0
+ * */
 int main(void)
 {
-	char first_var[] = "\"Programming is like building a multilingual puzzle\n";
+	char first_var[] = "\"Programming is like building a multilingual puzzle";
+
 	puts(first_var);
 
 	return (0);
