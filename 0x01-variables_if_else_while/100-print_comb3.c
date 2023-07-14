bd4f9a807dead6f775274866970c746401118088
Favour Eze <ezefavourchiamaka01@gmail.com>
2023-07-14 23:08:38 +0000

    task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -13,25 +13,18 @@ int main(void)
 
 	for (a = 0; a < 10; a++)
 	{
-		for (b = 0; b < 10; b++)
+		for (b = a+1; b < 10; b++)
 		{
-			if (a != b)
+			putchar(a + '0');
+			putchar(b + '0');
+
+			if (a != 8 || b != 9)
 			{
-				if (a < b)
-				{
-					putchar(a + '0');
-					putchar(b + '0');
-					putchar(',');
-				}
-				else
-				{
-					putchar(b + '0');
-					putchar(a + '0');
-					putchar(',');
-				}
+				putchar(',');
+				putchar(' ');
 			}
-				
 		}
 	}
+	putchar('\n');
 	return (0);
 }
