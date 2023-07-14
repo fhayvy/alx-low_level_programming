c3dda3e232775cbe60db0892d7cfa186181ce564
Favour Eze <ezefavourchiamaka01@gmail.com>
2023-07-14 17:27:35 +0000

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int ch;
+
+	for (ch = 0; ch <= 9; ch++)
+	{
+		printf("%d", ch);
+	}
+
+	printf('\n');
+
+	return (0);
+}
