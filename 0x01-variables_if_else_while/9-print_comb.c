55d2efa900a1b0fe8421e7adf29de0029c52af6c
Favour Eze <ezefavourchiamaka01@gmail.com>
2023-07-14 19:24:57 +0000

    task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,16 +9,11 @@
 int main(void)
 {
 	int a;
-	int b;
 
 	for (a = 0; a < 10; a++)
 	{
-		for (b = 0; b < 10; b++)
-		{
-			putchar(a + '0');
-			putchar(b + '0');
-			putchar(',');
-		}
+		putchar(a + '0');
+		putchar(',');
 	}
 	putchar('\n');
 
