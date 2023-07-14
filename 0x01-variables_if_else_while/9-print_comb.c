35647adb1fdfe32fc2cf8321531c29996bc599ef
Favour Eze <ezefavourchiamaka01@gmail.com>
2023-07-14 19:06:01 +0000

    task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int a;
+	int b;
+
+	for (a = 0; a < 10; a++)
+	{
+		for (b = 0; b < 10; b++)
+		{
+			putchar(a + '0');
+			putchar(b + '0');
+			putchar(',');
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
