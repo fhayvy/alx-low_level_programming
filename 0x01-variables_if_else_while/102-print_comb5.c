55b6992214ebfe169c36678ce914487590dd5952
Favour Eze <ezefavourchiamaka01@gmail.com>
2023-07-14 22:18:41 +0000

    task 12

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int a;
+	int b;
+
+	for (a = 0; a < 10; a++)
+	{
+		for (b = 0; b < 10; b++)
+		{
+			putchar(a + '0');
+			putchar(b + '0');
+			putchar(' ');
+			putchar(b + '0');
+			putchar(a + '0');
+
+			if (a != 9 && b != 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
