88c06c72e58217b37a9f649762cda16c143bcd34
Favour Eze <ezefavourchiamaka01@gmail.com>
2023-07-15 00:39:09 +0000

    task 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -11,16 +11,16 @@ int main(void)
 	int a;
 	int b;
 
-	for (a = 0; a < 100; a++)
+	for (a = 0; a <= 98; a++)
 	{
-		for (b = a + 1; b < 100; b++)
+		for (b = a + 1; b <= 99; b++)
 		{
 			putchar((a / 10) + '0');
 			putchar((b / 10) + '0');
 			putchar(' ');
 			putchar((a % 10) + '0');
 			putchar((b % 10) + '0');
-			if (!(a == 99 && b == 99))
+			if (!(a == 98 && b == 99))
 			{
 				putchar(',');
 				putchar(' ');
