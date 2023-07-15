1041e5077d042df099bdd207e395f1feadf9f0c9
Favour Eze <ezefavourchiamaka01@gmail.com>
2023-07-15 00:14:27 +0000

    task 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -20,7 +20,7 @@ int main(void)
 			putchar(' ');
 			putchar((b / 10) + '0');
 			putchar((b % 10) + '0');
-			if (a != 99 && b != 98)
+			if (a != 99 || b != 99)
 			{
 				putchar(',');
 				putchar(' ');
