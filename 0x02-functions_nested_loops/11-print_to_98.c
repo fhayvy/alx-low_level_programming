fb2cd240ee699922cec9da3b10f294b4d8248536
Favour Eze <ezefavourchiamaka01@gmail.com>
2023-07-18 21:07:02 +0000

    task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,16 +9,34 @@
 
 void print_to_98(int n)
 {
-	while (n != 98)
+	if (n <= 98)
 	{
-		_putchar((n / 10) + '0');
-		_putchar((n % 10) + '0');
-		n++;
-
-		if (n < 98)
+		while (n <= 98)
 		{
-			_putchar(',');
-			_putchar(' ');
+			_putchar((n / 10) + '0');
+			_putchar((n % 10) + '0');
+			n++;
+			
+			if (n !=98)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+	}
+	else
+	{
+		while (n >= 98)
+		{
+			_putchar((n / 100) + '0');
+			_putchar((n / 10) % 10 + '0');
+			n--;
+
+			if (n != 98)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
 		}
 	}
 }
