5dfacde969eb11107c04eec01e3f6f2c2d0cd563
Favour Eze <ezefavourchiamaka01@gmail.com>
2023-07-18 17:23:28 +0000

    task 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_last_digit - Prints the last digit of an alphabet
+ * @n: the integer
+ *
+ * Return: value of the last digit
+ */
+
+int print_last_digit(n)
+{
+	int ch;
+
+	ch = n % 10;
+
+	return (ch);
+}
