243dfd6e48b313867e002daa36f3993387a97d4f
Favour Eze <ezefavourchiamaka01@gmail.com>
2023-07-18 20:01:02 +0000

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * add - Entry point
+ * @a: First integer
+ * @b: Second integer
+ *
+ * Return: The sum of two integers
+ */
+
+int add(int a, int b)
+{
+	int a;
+	int b;
+	int ch;
+
+	ch = a + b
+
+	_putchar(ch + '0');
+	return (0);
+
+}
