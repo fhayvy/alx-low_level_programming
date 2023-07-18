4e1f1c7ea64f8ce93c1d84fc6b3596496797ca0d
Favour Eze <ezefavourchiamaka01@gmail.com>
2023-07-18 19:43:39 +0000

    task 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * times_table - Entry point
+ *
+ * Return: Always 0 (Sucess)
+ */
+
+void times_table(void)
+{
+	int a;
+	int b;
+
+	for (a = 0; a < 10; a++)
+	{
+		int ch;
+
+		for (b = 0; b < 10; b++)
+		{
+			ch = (a * b);
+
+			_putchar(ch);
+		}
+
+		_putchar('\n');
+	}
+}
