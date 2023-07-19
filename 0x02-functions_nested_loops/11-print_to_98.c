acbd77cca2b3b7d1353630f1be744e3cc4589dde
Favour Eze <ezefavourchiamaka01@gmail.com>
2023-07-19 09:44:08 +0000

    task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,7 +12,7 @@ void print_to_98(int n)
 {
 	if (n > 98)
 	{
-		while (n > 98)
+		while (n >= 98)
 		{
 			printf("%d", n);
 			if (n != 98)
@@ -25,7 +25,7 @@ void print_to_98(int n)
 	}
 	else if (n < 98)
 	{
-		while (n < 98)
+		while (n <= 98)
 		{
 			printf("%d", n);
 			if (n != 98)
@@ -39,6 +39,7 @@ void print_to_98(int n)
 	else
 	{
 		printf("%d", n);
+		printf("\n");
 	}
 }
 
