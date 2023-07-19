18147e8e89ad4ccd48cbabf92a9778831062161a
Favour Eze <ezefavourchiamaka01@gmail.com>
2023-07-19 11:25:33 +0000

    task 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -10,11 +10,14 @@
 
 void print_times_table(int n)
 {
+	int a, b, ch;
+
 	for (a = 0; a <= 15; a++)
 	{
 		for (b = 0; b <= 15; b++)
 		{
 			ch = (a * b);
+
 			if (ch > 100)
 			{
 				_putchar((ch / 100) + '0');
