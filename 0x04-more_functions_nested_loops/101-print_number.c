b18262a80bd6ca0aaf841d282ff7009249f050cd
Favour Eze <ezefavourchiamaka01@gmail.com>
2023-07-20 21:53:31 +0000

    task 12

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,50 +1,20 @@
 #include "main.h"
 
 /**
- * print_number - Print any number one character at a time
- * @n: Number to print
- *
- * Return: Nothing
+ * print_number - Prints an integer
+ * @n: The integer to be printed
  */
 void print_number(int n)
 {
-	long hn;
-	long n2 = n;
+    if (n < 0)
+    {
+        _putchar('-');
+        n = -n;
+    }
 
-	if (n2 < 0)
-	{
-		_putchar('-');
-		n2 *= -1;
-	}
-	if (n2 / 100000 != 0)
-	{
-		hn = n2 / 100000;
-		_putchar(hn / 10000 % 10 + '0');
-		_putchar(hn / 1000 % 10 + '0');
-		_putchar(hn / 100 % 10 + '0');
-		_putchar(hn / 10 % 10 + '0');
-		_putchar(hn % 10 + '0');
-	}
-		hn = n2 % 100000;
-		if (hn / 10000 % 10 != 0)
-		{
-			_putchar(hn / 10000 % 10 + '0');
-			_putchar(hn / 1000 % 10 + '0');
-			_putchar(hn / 100 % 10 + '0');
-			_putchar(hn / 10 % 10 + '0');
-		}
-		else if (hn / 1000 % 10 != 0)
-		{
-			_putchar(hn / 1000 % 10 + '0');
-			_putchar(hn / 100 % 10 + '0');
-			_putchar(hn / 10 % 10 + '0');
-		}
-		else if (hn / 100 % 10 != 0)
-		{
-			_putchar(hn / 100 % 10 + '0');
-			_putchar(hn / 10 % 10 + '0');
-		}
-		else if (hn / 10 % 10 != 0)
-			_putchar(hn / 10 % 10 + '0');
-		_putchar(hn % 10 + '0');
+    if (n / 10 != 0)
+        print_number(n / 10);
+
+    _putchar((n % 10) + '0');
 }
+
