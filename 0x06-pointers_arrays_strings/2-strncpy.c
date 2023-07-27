96d734e260f21f9597119b2eb8e6754fc8b5c919
Favour Eze <ezefavourchiamaka01@gmail.com>
2023-07-27 21:25:38 +0000

    task 3

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * *_strncpy - Copies a string
+ *
+ * @dest: String 1
+ * @src: String 2
+ * @n: Number of characters to be copied from string 1 to string 2
+ *
+ * Return: a character pointer 
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+
---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strcmp - Compares two strings
+ * @s1: String 1
+ * @s2: String 2
+ *
+ * Return:An Integer
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int index = 0;
+	int result = *s1 - *s2;
+
+	while ((*s1 != '\0') && (*s2 != '\0'))
+	{
+		if (s1[index] != s2[index])
+		{
+			return result;
+		}
+		else if (*s1 == *s2)
+		{
+			return (0);
+		}
+	s1++;
+	s2++;
+	}
+	return result;
+}
