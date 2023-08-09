ffaacb48c24bdc7251545a9e9d71faed2c9fb04b
Favour Eze <ezefavourchiamaka01@gmail.com>
2023-08-09 19:59:12 +0000

    task 6

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,45 +1,98 @@
 #include "main.h"
 #include <stdlib.h>
 
-
 /**
- * strtow - Splits a string into words
- *
+ * word_len - Calculates the length of chars in a word
  * @str: The string
- * Return: A pointer to an array of strings
+ *
+ * Return: The length of words
  */
 
-int words_len(char *str)
+
+int word_len(char *str)
+{
+	int len = 0;
+	int i = 0;
+
+	while (str[i] != ' ' && str[i] != '\0')
+	{
+		len++;
+		i++;
+	}
+	return (len);
+}
+
+/**
+ * word_count - Counts the number of words in a string
+ * @str: The string
+ *
+ * Return: The number of words
+ */
+
+
+int word_count(char *str)
 {
 	int words = 0, i = 0;
+
 	while (str[i] != '\0')
 	{
 		while (str[i] != ' ' && (str[i + 1] != ' ' || str[i + 1] != '\0'))
+		{
+			i++;
+		}
+		if (str[i] == ' ')
 		{
 			words++;
 		}
 		i++;
 	}
-	return (words);
+	return (words + 1);
 }
 
+/**
+ * strtow - Splits a string into words
+ * @str: The string
+ *
+ * Return: A pointer to an array of strings
+ */
 
-	
+
 char **strtow(char *str)
 {
 	char **a;
+	int count = word_count(str);
+	int i = 0, k, l;
 
 	if (str == NULL || *str == '\0')
-		return NULL;
-	a = malloc(sizeof(char *) * (words_len + 1));
+		return (NULL);
+	a = malloc(sizeof(char *) * (count + 1));
 
 	if (a == NULL)
 		return (NULL);
-
 	while (str[i] != '\0')
 	{
-	
+		int len = word_len(&str[i]);
+
+		a[i] = malloc(sizeof(char) * (len + 1));
+
+		if (a[i] == NULL)
+		{
+			for (k = 0; k < i; k++)
+			{
+				free(a[k]);
+			}
+			free(a);
+			return (NULL);
+		}
+
+		for (l = 0; l < len; l++)
+		{
+			a[i][l] = str[i];
+		}
+		a[i][l] = '\0';
+		i++;
 	}
+	a[count] = NULL;
 
 	return (a);
 }
