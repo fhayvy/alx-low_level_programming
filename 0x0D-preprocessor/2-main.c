6b7838eab16676177956960ee79a6c9c8fbcf66d
Favour Eze <ezefavourchiamaka01@gmail.com>
2023-08-14 16:55:47 +0000

    task 2

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints the name of the file it was complied from
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf(__FILE__);
+	printf("\n");
+	return (0);
+}
