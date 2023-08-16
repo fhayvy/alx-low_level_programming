d0e215044cd5e52f37cb2596248fbaa3433fbf21
Favour Eze <ezefavourchiamaka01@gmail.com>
2023-08-16 13:23:31 +0000

    task 0 fix

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,5 +1,10 @@
+#ifndef HEADER_NAME_H
+#define HEADER_NAME_H
 #include <stddef.h>
 
 
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
+
+
+#endif
