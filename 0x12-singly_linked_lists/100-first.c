058ed0ad67ac4e8f86801bfe737ec7f6926b3b53
Favour Eze <ezefavourchiamaka01@gmail.com>
2023-08-25 20:36:30 +0000

    task 5

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+
+/**
+ * bmain - Function executed before main
+ * Return: None
+ */
+
+
+void __attribute__((constructor)) bmain()
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
