6e8c95dfbc1b5999c0b57aa3446b098271f30288
Favour Eze <ezefavourchiamaka01@gmail.com>
2023-08-25 19:13:35 +0000

    task 3

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -2,7 +2,7 @@
 
 
 /**
- * add_node - Adds a ne node at the beginning of a list_t list
+ * add_node_end - Adds a new node at the end of a list_t list
  * @head: Head of the linked list
  * @str: String to store in the list
  *
@@ -10,14 +10,16 @@
  */
 
 
-list_t *add_node(list_t **head, const char *str)
+list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *new_node = malloc(sizeof(list_t));
+	list_t *temp;
 
 	if (new_node == NULL)
 		return (NULL);
 
 	new_node->str = strdup(str);
+
 	if (new_node->str == NULL)
 	{
 		free(new_node);
@@ -25,8 +27,18 @@ list_t *add_node(list_t **head, const char *str)
 	}
 
 	new_node->len = strlen(str);
-	new_node->next = *head;
-	*head = new_node;
+	new_node->next = NULL;
+	temp = *head;
+
+	if (temp == NULL)
+		*head = new_node;
+
+	else
+	{
+		while (temp->next != NULL)
+			temp = temp->next;
+		temp->next = new_node;
+	}
 
 	return (*head);
 }
