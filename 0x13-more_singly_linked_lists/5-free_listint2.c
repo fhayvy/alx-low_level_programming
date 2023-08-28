8592a6c064160e8ce157a1e1a3d072cead1028a3
Favour Eze <ezefavourchiamaka01@gmail.com>
2023-08-28 22:51:46 +0000

    task 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+
+/**
+ * free_listint2 - Frees a listint_t list
+ * @head: A pointer to the head pointer
+ */
+
+
+void free_listint2(listint_t **head)
+{
+	listint_t *current;
+
+	while (*head != NULL)
+	{
+		current = *head;
+		*head = current->next;
+
+		free(current);
+	}
+
+	head = NULL;
+}
