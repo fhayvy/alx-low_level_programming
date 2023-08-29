4dff6f2f4faef26f1798f0b6ee2ea4b17d3362ba
Favour Eze <ezefavourchiamaka01@gmail.com>
2023-08-29 08:57:43 +0000

    task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -26,7 +26,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		}
 	}
 
-	if (*head == NULL && idx != 0)
+	if (current == NULL && idx != 0)
 		return (NULL);
 
 	new_node = malloc(sizeof(listint_t));
