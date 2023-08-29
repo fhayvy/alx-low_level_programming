32217171f5a110cf8b7c4bf55218e3059f1d4851
Favour Eze <ezefavourchiamaka01@gmail.com>
2023-08-29 22:28:29 +0000

    task 13

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -2,12 +2,72 @@
 
 
 /**
+ * free_listp2 - Frees a linked list
+ * @head: Head of the LL
+ */
+
+void free_listp2(listp_t **head)
+{
+	listp_t *temp, *current;
+
+	if (head != NULL)
+	{
+		current = *head;
+		while ((temp = current) != NULL)
+		{
+			current = current->next;
+			free(temp);
+		}
+		*head = NULL;
+	}
+}
+
+
+/**
  * print_listint_safe - Prints a listint_t LL
- * @head: The head pointer
+ * @h: The head pointer
  *
  * Return: The number of nodes in the list
  */
 
-size_t print_listint_safe(const listint_t *head)
+size_t free_listint_safe(listint_t **h)
 {
+	size_t nnodes = 0;
+	listp_t *head_ptr, *new, *add;
+	listint_t *current;
 
+	head_ptr = NULL;
+	while (*h != NULL)
+	{
+		new = malloc(sizeof(listp_t));
+
+		if (new == NULL)
+			exit(98);
+
+		new->p = (void *)*h;
+		new->next = head_ptr;
+		head_ptr = new;
+
+		add = head_ptr;
+
+		while (add->next != NULL)
+		{
+			add = add->next;
+			if (*h == add->p)
+			{
+				*h = NULL;
+				free_listp2(&head_ptr);
+				return (nnodes);
+			}
+		}
+
+		current = *h;
+		*h = (*h)->next;
+		free(current);
+		nnodes++;
+	}
+
+	*h = NULL;
+	free_listp2(&head_ptr);
+	return (nnodes);
+}
