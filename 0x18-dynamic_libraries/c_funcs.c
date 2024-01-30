1222e44ed703561a3e8912c86d3c94c51ddc9f87
Favour Eze <ezefavourchiamaka01@gmail.com>
2024-01-30 16:53:45 +0000

    task 2

---
+++ 0x18-dynamic_libraries/c_funcs.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+
+/**
+ * add - Adds two integers
+ * @a: First Integer
+ * @b: Second Integer
+ * Return: The addition of both integers
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+
+/**
+ * sub - Subtarction
+ * @a: First Integer
+ * @b: Second Integer
+ * Return: The subraction
+ */
+
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+
+/**
+ * mul - Multiplies two integers
+ * @a: First Integer
+ * @b: Second Integer
+ * Return: The multiplication of both integers
+ */
+
+int mul(int a, int b)
+{
+	return (a *b);
+}
+
+
+/**
+ * div - Division
+ * @a: First Integer
+ * @b: Second Integer
+ * Return: The result of the division
+ */
+
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+
+/**
+ * mod - The remainder when an integer divides another
+ * @a: First Integer
+ * @b: Second Integer
+ * Return: The result
+ */
+
+int mod(int a, int b)
+{
+	return (a % b);
+}
+
