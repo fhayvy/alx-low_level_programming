8d4c0a52ce43754cb31b5c94e00c9b59dc6a3db3
Favour Eze <favour@Favour-Eze.local>
2024-07-19 15:39:36 +0100

    task 10

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -10,5 +10,6 @@ int binary_search(int *array, size_t size, int value);
 void print_array(int *arr, int a, int z);
 int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
+int exponential_search(int *array, size_t size, int value);
 
 #endif
